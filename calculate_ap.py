5d3b97f4f14d0ffa8e861400f944c328c49d984a
simenvg <simen.grini@gmail.com>
2018-02-20 10:38:46 +0100

    fixed bug with max and min points in ground truth boxes

--- calculate_ap.py
+++ calculate_ap.py
@@ -11,23 +11,25 @@ GREEN = (0,255,0)
 YELLOW = (255,255,0)
 
 
-box_1 = [(0, 0), (5, 5)]
-box_2 = [(1, 1), (5, 7)]
-# intersection here is (3, 3, 4, 3.5), or an area of 1*.5=.5
+
 
 def intersect_area(box_1, box_2):  # returns None if rectangles don't intersect
-	dx = min(box_1[1][0], box_2[1][0]) - max(box_1[0][0], box_2[0][0])
-	dy = min(box_1[1][1], box_2[1][1]) - max(box_1[0][1], box_2[0][1])
+	dx = min(max(box_1[1][0], box_1[0][0]), box_2[1][0]) - max(min(box_1[1][0], box_1[0][0]), box_2[0][0])
+	dy = min(max(box_1[1][1], box_1[0][1]), box_2[1][1]) - max(min(box_1[1][1], box_1[0][1]), box_2[0][1])
 	if (dx>=0) and (dy>=0):
 		return dx*dy
 	else:
 		return -1
 
+#[(1670, 709), (1841, 585)] 
+#box_1:  [(1654, 678), (1854, 605)]    box_2:  [(1662, 628), (1890, 702)]
 
 def intersect_over_union(box_1, box_2):
-	area_box_1 = (box_1[1][0] - box_1[0][0]) * (box_1[1][1] - box_1[0][1])
-	area_box_2 = (box_2[1][0] - box_2[0][0]) * (box_2[1][1] - box_2[0][1])
+	print('box_1: ', box_1, '   box_2: ', box_2)
+	area_box_1 = abs((box_1[1][0] - box_1[0][0]) * (box_1[1][1] - box_1[0][1]))
+	area_box_2 = abs((box_2[1][0] - box_2[0][0]) * (box_2[1][1] - box_2[0][1]))
 	intersection = intersect_area(box_1, box_2)
+	print('area_box_1: ', area_box_1, '   area_box_2: ', area_box_2, '  intersection: ', intersection)
 	if intersection == -1:
 		return -1
 	else:
