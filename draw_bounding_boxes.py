645e9a43c609d4045629e05bd5fdf7d817b93545
simenvg <simen.grini@gmail.com>
2018-04-04 14:21:07 +0200

    Added functionality to add boxes from Faster R-CNN

--- draw_bounding_boxes.py
+++ draw_bounding_boxes.py
@@ -4,16 +4,17 @@ import os
 
 
 
-folder_path = '/home/simenvg/environments/my_env/prosjektoppgave/boats'
+folder_path = '/home/simenvg/environments/my_env/prosjektoppgave/Dataset/dark_sea'
 
 
 RED = (0,0,255)
 GREEN = (0,255,0)
-
+BLUE = (255,0,0)
 
 
 images_GT = pickle.load(open(os.path.join(folder_path,'ground_truth.txt'), "rb"))
 images_YOLO = pickle.load(open(os.path.join(folder_path,'YOLO.txt'), "rb"))
+images_RCNN = pickle.load(open(os.path.join(folder_path,'dark_sea_rcnn.txt'), "rb"))
 
 filenames = []
 
@@ -28,5 +29,8 @@ for image in filenames:
 		cv2.rectangle(img, box[0], box[1], RED, 1)
 	for box in images_GT[image]:
 		cv2.rectangle(img, box[0], box[1], GREEN, 1)
-	cv2.imshow('image', img)
-	cv2.waitKey(0)
+	#for box in images_RCNN[image]:
+	#	cv2.rectangle(img, box[0], box[1], BLUE, 1)
+	#cv2.imshow('image', img)
+	cv2.imwrite("res_yolo_" + image, img)
+	#cv2.waitKey(0)
---
+++ matlab_to_py.py
@@ -0,0 +1,65 @@
+import os
+import pickle
+
+
+folderpath = '/home/simenvg/environments/my_env/prosjektoppgave/score_box/light_sea_rcnn'
+
+images = {}
+
+scores_path = folderpath + '/scores'
+boxes_path =  folderpath + '/boxes'
+
+print(scores_path)
+
+scores = sorted(os.listdir(folderpath + '/scores'))
+boxes  = sorted(os.listdir(folderpath + '/boxes'))
+
+
+for i in range(len(scores)):
+    print(scores[i])
+    print(boxes[i])
+
+
+
+
+images ={}
+
+for i in range(len(scores)):    # Looper gjennom bilder
+    print('########################')
+    approved_boxes = []
+
+    with open(os.path.join(scores_path, scores[i])) as f:
+        #print('hei')
+        image_scores = f.readlines()
+    image_scores = [x.strip() for x in image_scores] 
+    
+    with open(os.path.join(boxes_path, boxes[i])) as fi:
+        image_boxes = fi.readlines()
+    image_boxes = [x.strip() for x in image_boxes]
+
+    for j in range(len(image_scores)):
+        if float(image_scores[j]) >= 0.25:
+            #print(image_scores[j])
+            #print('GDFSHFH')
+            approved_boxes.append(image_boxes[j])
+    #print(approved_boxes)
+
+    filename = scores[i][:-4]
+    filename = filename.replace("_scores", "")
+    filename = filename + ".jpg"
+    print(filename)
+    # [(left, top), (right, bottom)]
+
+    objects = []
+
+    for elem in approved_boxes:
+        elem = elem.replace("'", "")
+        lst = elem.split(",")
+        
+        objects.append([(int(float(lst[0])), int(float(lst[1]))), (int(float(lst[2])), int(float(lst[3])))])
+        print(objects)
+
+    images[filename] = objects
+    #print(images)
+
+pickle.dump(images, open(os.path.join(folderpath,'light_sea_rcnn.txt'), "wb"))
